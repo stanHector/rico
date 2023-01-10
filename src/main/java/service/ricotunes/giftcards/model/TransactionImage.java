1b78a79e65f0c56926b7eef328eecf505d790daa
Bazz <abbasirekeola@gmail.com>
2023-01-10 12:13:24 -0600

    added transaction image

---
+++ src/main/java/service/ricotunes/giftcards/model/TransactionImage.java
@@ -0,0 +1,23 @@
+package service.ricotunes.giftcards.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "transaction_image")
+public class TransactionImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinTable(name = "transaction",
+            joinColumns = @JoinColumn(name = "transaction_image_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "transactions", referencedColumnName = "id"))
+    private Transactions transaction;
+
+    private String image;
+}
