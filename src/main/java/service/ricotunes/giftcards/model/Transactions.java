e33c4ab28f16603036d2a58e5fd1dbe1f496621c
Hector Stanley <63668294+stanHector@users.noreply.github.com>
2023-01-23 09:46:33 +0100

    added giftcard model to transactions

--- src/main/java/service/ricotunes/giftcards/model/Transactions.java
+++ src/main/java/service/ricotunes/giftcards/model/Transactions.java
@@ -13,6 +13,7 @@ public class Transactions {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    private String date;
     private double quantity;
     private double amount;
 
@@ -20,7 +21,13 @@ public class Transactions {
     @ElementCollection(targetClass=String.class)
     private List<String> imageList;
     private Long userId;
-    private Long giftCardId;
     private String status;
     private String remarks;
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinTable(name = "transactions_giftCard",
+            joinColumns = @JoinColumn(name = "transactions_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "giftCard_id", referencedColumnName = "id"))
+    private GiftCard giftCard;
 }
