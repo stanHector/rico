d0fb0bdc7cd83c28f6847a02b36541343387943d
Hector Stanley <63668294+stanHector@users.noreply.github.com>
2023-03-11 11:50:12 +0100

    commit

--- src/main/java/service/ricotunes/giftcards/repository/TransactionRepository.java
+++ src/main/java/service/ricotunes/giftcards/repository/TransactionRepository.java
@@ -4,12 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import service.ricotunes.giftcards.model.Transactions;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long> {
 
     Transactions findByUserId(Long userId);
 
-//    List<Transactions> getAllByUserId(Long userId);
+    List<Transactions> getAllByUserId(Long userId);
 
 //    List<Transactions> findByUserId(Long userId);
 }
