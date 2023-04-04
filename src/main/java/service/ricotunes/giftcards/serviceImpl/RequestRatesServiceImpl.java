1bb7baad12003f8d082325096ff612e4d3800bec
Hector Stanley <63668294+stanHector@users.noreply.github.com>
2023-04-04 07:23:57 +0100

    commit

--- src/main/java/service/ricotunes/giftcards/serviceImpl/RequestRatesServiceImpl.java
+++ src/main/java/service/ricotunes/giftcards/serviceImpl/RequestRatesServiceImpl.java
@@ -26,6 +26,7 @@ public class RequestRatesServiceImpl implements RequestRatesService {
         requests.setType(requestRates.getType());
         requests.setCategory(requestRates.getGiftCard().getDenomination());
         requests.setUserId(requestRates.getUserId());
+        requests.setGiftCard(requestRates.getGiftCard());
         requests.setCountry(String.valueOf(requestRates.getGiftCard().getCategory().getCountry().getCurrency()));
         requests.setComment(requestRates.getComment());
         return requestRateRepository.save(requests);
